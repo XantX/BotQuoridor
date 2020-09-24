5604a24bc3afa5f9eb5406c9805ce711d52b5dfe
Sebastian <u201717471@upc.edu.pe>
2020-09-23 21:53:28 -0500

    algoritmos A* con networkX

--- src/Test/A_star.py
+++ src/Test/A_star.py
@@ -1,1 +1,19 @@
-print("Hello")
+import networkx as nx
+import matplotlib.pyplot as plt
+G = [[0, 1, 0, 1],
+     [0, 0, 0, 1],
+     [0, 1, 0, 1],
+     [1, 1, 0, 0]]
+def showG(G, directed = True):
+    g = nx.DiGraph() if directed else nx.Graph()
+    n = len(G)
+    g.add_nodes_from([u for u in range(n)])
+    for u in range(n):
+        for v in range(n):
+            if G[u][v] != 0:
+                g.add_edge(u, v)
+    print(list(nx.astar_path(g, 2, 0, weight = None)))
+    nx.draw(g, with_labels = True)
+    plt.show()
+
+showG(G)
