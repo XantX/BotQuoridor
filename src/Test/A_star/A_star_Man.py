9f8b9f007185934c486e956730575ed64ab28891
Sebastian <u201717471@upc.edu.pe>
2020-09-28 17:18:08 -0500

    Implementacion de A* con reconstruccion de camino sin refactorizacion de codigo

--- src/Test/A_star/A_star_Man.py
+++ src/Test/A_star/A_star_Man.py
@@ -16,10 +16,10 @@ class A_star:
         self.nEnd = (nodoEndx , nodoEndy)
 
     def Neighbor(self, mat):
-        dx = [1, -1, 0, 0] 
-        dy = [0, 0, 1, -1]
+        dx = [-1, 0, 1, 0]
+        dy = [0, 1, 0, -1]
         n = len(mat)
-        oSet = PriorityQueue() 
+        oSet = ListaMod() 
         cSet = [False]*(n*n)
         cPath = PriorityQueue() 
         Xg = self.nIn[0]
@@ -28,27 +28,79 @@ class A_star:
             n = len(mat)
             return Xg + dx[i] < n and Yg + dy[i] < n and not cSet[mat[Xg + dx[i]][Yg + dy[i]].NodeNumber]
 
-        oSet.put(mat[Xg][Yg])
+        oSet.add(mat[Xg][Yg])
         terminado = False
-        input()
-        while not oSet.empty() and terminado != True:
-            Ganador = oSet.get()
+        while not oSet.emptyS() and terminado != True:
+            Ganador = oSet.MayorF()
             Xg = Ganador.XC
             Yg = Ganador.YC
             print("Nodo: ",Xg, Yg, "F: ",Ganador.fC)
             input()
-            cPath.put(Ganador)
             cSet[mat[Xg][Yg].NodeNumber] = True
             for i in range(4):
                     if valid(i):
-                        mat[Xg + dx[i]][Yg + dy[i]].SethC(self.nEnd[0], self.nEnd[1], mat[Xg][Yg].gC)
-                        if (Xg + dx[i]) == self.nEnd[0] and (Yg + dy[i]) == self.nEnd[1]:
-                            terminado = True
-                            cPath.put(mat[Xg + dx[i]][Yg + dy[i]])
-                            print("Encontrado")
-                            break
-                        oSet.put(mat[Xg + dx[i]][Yg + dy[i]])
+                        if oSet.count(mat[Xg + dx[i]][Yg + dy[i]]):
+                            oSet.update(Ganador, mat[Xg + dx[i]][Yg + dy[i]])
+                        else:
+                            #Se quedo en el enlace de padres
+                            mat[Xg + dx[i]][Yg + dy[i]].SethC(self.nEnd[0], self.nEnd[1], Ganador)
+                            if (Xg + dx[i]) == self.nEnd[0] and (Yg + dy[i]) == self.nEnd[1]:
+                                terminado = True
+                                Final = mat[Xg + dx[i]][Yg + dy[i]]
+                                print("Encontrado")
+                                break
+                            oSet.add(mat[Xg + dx[i]][Yg + dy[i]])
+        print(Final.NodeNumber, Final.gC) 
+        Path = Final.Padre
+        cPath.put(Final)
+        while Path.Padre != None:
+            print(Path.NodeNumber, Path.gC)
+            Path = Path.Padre
+            cPath.put(Path)
+
         return cPath
+class ListaMod:
+    def __init__(self):
+        self.Arreglo = []
+        self.size = 0
+
+    def emptyS(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def add(self, objeto):
+        self.Arreglo.append(objeto)
+        self.size += 1
+
+    def count(self, objeto):
+        for i in self.Arreglo:
+            if(i.NodeNumber == objeto.NodeNumber):
+                return True
+        return False
+
+    def MayorF(self):
+        #error
+        menor = self.Arreglo[0]
+        cont = 0
+        for j in self.Arreglo:
+            print("indice:", j.NodeNumber, j.fC)
+            if menor.fC > j.fC:
+                menor = j
+
+        aux = self.Arreglo.pop(self.Arreglo.index(menor))
+        print("salio de OS: ", aux.NodeNumber)
+        self.size -= 1
+
+        return menor
+
+    def update(self, Ganador, objeto):
+        for i in self.Arreglo:
+            if i.NodeNumber == objeto.NodeNumber:
+                if i.gC > Ganador.gC + 1:
+                    i.SethCUp(Ganador)
+                    break
 
 class Casilla(object):
     def __init__(self, xC, yC, nN):
@@ -58,16 +110,23 @@ class Casilla(object):
         self.gC = 0
         self.hC = 0
         self.fC = 0 
+        self.Padre = None
 
-    def SethC(self, xO, yO, g):
-        self.gC = 1 + g 
+    def SethC(self, xO, yO, casilla):
+        self.gC = 1 + casilla.gC 
+        self.Padre = casilla
         dx = self.XC - xO
         dy = self.YC - yO
         self.hC = abs(dx) + abs(dy) 
         self.fC = self.gC + self.hC
 
+    def SethCUp(self, Ganador):
+        self.gC = 1 + Ganador.gC 
+        self.fC = self.gC + self.hC
+        self.Padre = Ganador 
+
     def __lt__(self, otro):
-        return self.fC < otro.fC
+        return self.gC < otro.gC
 
 class tablero:
     def __init__(self, X, Y):
@@ -92,12 +151,6 @@ class tablero:
 tabla = tablero(9, 9)
 tabla.createTable()
 tabla.viewTable()
-print("quiero llegar a: 0, 0")
-Algoritmo = A_star(2, 1, 0, 2)
+print("quiero llegar a: 0, 2")
+Algoritmo = A_star(8, 4, 0, 4)
 Path = Algoritmo.Neighbor(tabla.mat)
-cont = 0
-while not Path.empty():
-    c = Path.get()
-    cont += 1
-    print(c.NodeNumber)
-print("Cantidad:",cont)
